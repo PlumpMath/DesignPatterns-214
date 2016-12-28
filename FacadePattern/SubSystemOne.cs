984c2301847bbf302e938ad9ed3a2ee34eac0f87
Marcelo Diniz <marcelodinizsl@gmail.com>
2016-12-28 19:44:15 -0400

    - Add: Facade and Singleton patterns

---
+++ FacadePattern/SubSystemOne.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePattern
+{
+    class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine("SubSystemOne Method");
+        }
+    }
+
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine("SubSystemTwo Method");
+        }
+    }
+
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine("SubSystemThree Method");
+        }
+    }
+
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine("SubSystemFour Method");
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1==s2)
+                Console.WriteLine("Object are the same instance");
+        }
+    }
+}
